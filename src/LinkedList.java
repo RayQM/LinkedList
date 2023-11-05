cca05a37cffcdf87b09a773e151045e9ff20c1c9
Qiaomu Lei <115581793+RayQM@users.noreply.github.com>
2023-11-05 13:44:36 -0700

    1111

---
+++ src/LinkedList.java
@@ -0,0 +1,106 @@
+import java.util.NoSuchElementException;
+
+public class LinkedList {
+    private class Node {
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+
+    }
+    private Node first;
+    private  Node last;
+
+
+    public void addLast1(int item){
+        var node = new Node(item);
+
+        if (isEmpty())
+            first = last =node;
+        else {
+            last.next = node;
+            last = node;
+        }
+    }
+
+    public void addFirst(int item){
+        var node  = new Node(item);
+
+        if(isEmpty())
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+    }
+
+    public int indexOf(int item){
+        int index = 0;
+        var current = first;
+        while (current != null){
+            if (current.value == item) return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains (int item){
+        return indexOf(item) != -1;
+    }
+
+    public void removeFirst(){
+        if(isEmpty())
+            throw new NoSuchElementException();
+        if (first== last){
+            first = last = null;
+            return;
+        }
+        var second = first.next;
+        first.next = null;
+        first =second;
+    }
+
+
+
+    public void removeLast(){
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last){
+            first = last = null;
+            return;
+        }
+        var previous = getPrevious(last);
+        last = previous;
+        last.next = null;
+    }
+    public void removeIndex(int item){
+        Node current = first;
+        for (int i = 0; i== item-1; i++){
+            current = current.next;
+        }
+        Node prev = current;
+        Node removed = current.next;
+        prev.next = removed.next;
+
+    }
+
+
+
+    private Node getPrevious(Node node){
+        var current = first;
+        while (current != null){
+            if (current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    private boolean isEmpty(){
+        return first == null;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        var list = new LinkedList();
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+        list.removeIndex();
+        
+        System.out.println(list.contains(10));
+        }
+
+
+}
